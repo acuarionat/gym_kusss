a76421ab1bbf361e00a6d03df8f387feff2b0462
Carvajal Max <mxcarvajal2001@gmail.com>
2024-05-10 11:54:28 -0400

    SegundoComponete

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,15 @@ function App() {
 
   return (
     <>
-      <div className='card'></div>
+      <div className='card'>
+        <img src="./image/react2.png" alt="" className='imagen' />
+        <div className='boton'>
+          <div className='triangulo'></div>
+        </div>
+        <div className='Textos'>
+          {/* <p className='terapia'>Terapia 2</p> */}
+        </div>
+      </div>
     </>
   )
 }
