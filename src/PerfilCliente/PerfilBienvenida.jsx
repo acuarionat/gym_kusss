8de90934f3f34300c7f7ce75ac6ae98b0f0b629d
Natalia Lozano <acuarionat2004@gmail.com>
2024-05-31 13:24:20 -0400

    creacion_bienvenido

---
+++ src/PerfilCliente/PerfilBienvenida.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './PerfilBienvenida.css'
+
+const PerfilBienvenida = () => {
+  return (
+    <div className='bienvenida'>
+        <div className="texto">
+            <h3>Bien venido, <span className='rojo'>JuanDeLaCruz</span></h3>
+            <p>Este es tu espacio personalizado, donde podras encontrar las herramientas necesarias para cumplir tus objetivos.</p>
+        </div>
+        <div className="imagen">
+
+        </div>
+    </div>
+  )
+}
+
+export default PerfilBienvenida
