f23c7705fbe9f1c296831c2406b8c568898f54f5
Anderes <andres10000fky@gmail.com>
2024-06-06 11:42:06 -0400

    Componentes V6.1 Diseño Entrenador y Cliente (Iteraciones y combobox)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import VistaEditarRutinaEntrenador from './componentes/perfil entrenador/editar rutina/VistaEditarRutinaEntrenador'
 import "./App.css";
 
 function App() {
   return (
-    <></>
+    <>
+      <VistaEditarRutinaEntrenador />
+    </>
   );
 }
 
--- src/componentes/perfil entrenador/editar rutina/FormularioEditarRutina.jsx
+++ src/componentes/perfil entrenador/editar rutina/FormularioEditarRutina.jsx
@@ -11,10 +11,15 @@ function FormularioEditarRutina() {
             <div className='cajaTextoDia'>
                 <p className='textoDia'>Día:</p>
             </div>
-            <div className='cajaElegirDia'>
-                <p className='textoElejirDia'>Martes</p>
-                <img className='icoAñadir' src={flechaAbajo} alt="flechita" />
-            </div>
+            <select className='cajaElegirDia'>
+                <option className='textoElejirDia'>Lunes</option>
+                <option className='textoElejirDia'>Martes</option>
+                <option className='textoElejirDia'>Miercoles</option>
+                <option className='textoElejirDia'>Jueves</option>
+                <option className='textoElejirDia'>Viernes</option>
+                <option className='textoElejirDia'>Sábado</option>
+                <option className='textoElejirDia'>Domingo</option>
+            </select>
         </div>
     )
 }
