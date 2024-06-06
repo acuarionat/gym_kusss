395358b4e6a7b83ccd9f939ae74cdcd028425df8
edisonCheca <echecat@est.edu.bo>
2024-06-06 00:23:55 -0400

    antes de la eliminacion mapa-formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import './App.css';
-import BotonesInfo from './componentes/pagina/home/BotonesInformacion/BotonesInfo';
-import VistaNutricion from "./componentes/pagina/home/nutricion/VistaNutricion"
-import VistaPerdidaDeGrasa from './componentes/pagina/home/perdida de grasa/VistaPerdidaDeGrasa';
-import VistaSaludMental from './componentes/pagina/home/salud mental/VistaSaludMental';
-import TarjetaDeEquipamiento from './componentes/pagina/home/TarjetadeEquipamiento/VistaTarjetadeEquipamiento'
+
 
 function App() {
   return (
     <>
-    <TarjetaDeEquipamiento/>
     </>
   )
 }
