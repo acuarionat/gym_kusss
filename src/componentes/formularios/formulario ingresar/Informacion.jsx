63c3663e08c8daa56cd2572602060861951552f8
Miguel Flores <dragonex0020@gmail.com>
2024-06-06 09:08:42 -0400

    Modificando html

--- src/componentes/formularios/formulario ingresar/Informacion.jsx
+++ src/componentes/formularios/formulario ingresar/Informacion.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
+import React from 'react';
 import "./Informacion.css";
+
 const Informacion = () => {
   return (
     <div>
-      <div className='contenedor'>
-      <h2 className="titulo1">Ingresar</h2>
-      
-      <h3 className="parrafo">Correo</h3>
-      <input type="text" id='input' />
-      <h3 className="parrafo">Contraseña</h3>
-      <input type="text" id="input"/>
-      
-      <div className='button-container'>
-        <button className='button'><h2 className="titulob">ACEPTAR</h2></button>
-      </div>
+      <div className='contenedorLogin-informacion'>
+        <h2 className="titulo1-informacion">Ingresar</h2>
+        
+        <h3 className="parrafo-informacion">Correo</h3>
+        <input type="text" id='input-informacion' className='input-text-informacion' />
+        <h3 className="parrafo-informacion">Contraseña</h3>
+        <input type="text" id="input-informacion" className='input-text-informacion'/>
+        
+        <div className='button-container-informacion'>
+          <button className='button-informacion'><h2 className="titulob-informacion">ACEPTAR</h2></button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Informacion
+export default Informacion;
