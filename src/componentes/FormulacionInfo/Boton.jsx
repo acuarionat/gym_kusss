bc40f473d4ac7d428f05576dc86445c8f09aeade
edisonCheca <echecat@est.edu.bo>
2024-06-06 00:33:52 -0400

    version correjida V1

--- src/componentes/FormulacionInfo/Boton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './FormulacionInfo.css';
-
-const Boton = ({ type = "submit", text }) => (
-    <button type={type} className="submit-btn">{text}</button>
-);
-
-export default Boton;
