4943f92ae15a6b28d5e211bb348419a850b22ded
Natalia Lozano <acuarionat2004@gmail.com>
2024-06-07 13:50:14 -0400

    union completa v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import VistaClientesAsignados from "./componentes/perfil entrenador/clientes asignados/VistaClientesAsignados";
 function App() {
   return (
    <>
-    <VistaClientesAsignados/>
    </>
   );
 }
