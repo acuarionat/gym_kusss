7a9515811a487302cd2f8f09ff3490d1087dbaa4
Miguel Flores <dragonex0020@gmail.com>
2024-06-10 22:16:53 -0400

    Añadiendo Funcionalidad descargar

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,16 @@ import './App.css'
 import CardRegistro from './componentes/formularios/formulario registrar/CardRegistro'
 import CardLogin from './componentes/formularios/formulario ingresar/CardLogin'
 import VistaNosotros from './componentes/pagina/nosotros/VistaNosotros'
-import VistaEquipamiento from './componentes/pagina/home/equipamiento/VistaEquipamiento'
 import VistaHorario from './componentes/pagina/home/horario/VistaHorario'
-import VistaSugerencias from './componentes/pagina/home/sugerencias/VistaSugerencias'
-import VistaContactanos from './componentes/pagina/contactanos/VistaContactanos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* <VistaEquipamiento/> */}
-    {/* <VistaContactanos/> */}
+    {/* <CardRegistro/>
+    <CardLogin/> */}
     <VistaHorario/>
-    {/* <VistaSugerencias/> */}
     </>
   )
 }
