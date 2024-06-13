d47b671dd44d1908546537f780153f1aea648fa8
Miguel Flores <dragonex0020@gmail.com>
2024-06-12 22:13:05 -0400

    Añadiendo Envio de correo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import "./App.css";
-import PruebaSideBar from "./componentes/general/Slide Bar/PruebaSideBar";
-import BotonesInfo from "./componentes/pagina/home/botonesinformacion/VistaBotonesInfo";
-import VistaSeccionEquipamiento from "./componentes/pagina/home/seccioninformacion/VistaSeccionInformacion";
 import Navigation from "./routes/Navigation";
 function App() {
   return (
     <>
       <Navigation />
-      {/* <PruebaSideBar /> */}
     </>
   );
 }
--- src/componentes/pagina/home/sugerencias/CardSugerencias/Entrada.jsx
+++ src/componentes/pagina/home/sugerencias/CardSugerencias/Entrada.jsx
@@ -4,14 +4,24 @@ import "./Entrada.css";
 const Entrada = () => {
   return (
     <div className='contenedor'>
+    <form className='contenedor-form'
+      action="https://formspree.io/f/xbjnnkrb"
+      method="POST"
+    >
       <h2 className="titulo">SUGERENCIAS:</h2>
-      <input type="text" />
+      <input type="text" id="sugerencias" name="sugerencias" className="input-field" />
+  
       <h2 className="titulo">QUEJAS:</h2>
-      <input type="text" />
+      <input type="text" id="quejas" name="quejas" className="input-field" />
+  
       <div className='button-container'>
-        <button className='button'><h2 className="titulo">Enviar</h2></button>
+        <button type="submit" className='button'>
+          <h2 className="titulo">Enviar</h2>
+        </button>
       </div>
-    </div>
+    </form>
+  </div>
+  
   );
 }
 
