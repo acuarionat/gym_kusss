d412a6d2ac9544c56abebbdccde22c10c54b56e3
edisonCheca <echecat@est.edu.bo>
2024-06-14 01:09:27 -0400

    Correcion del carrusel a scroll

--- src/componentes/pagina/home/seccioninformacion/CabeceraInformate.jsx
+++ src/componentes/pagina/home/seccioninformacion/CabeceraInformate.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './CabeceraInformate.css'
 
-
 const CabeceraInformate = () => (
     <>
         <div className="ContainerTitulo">
             <hr className='linea'></hr>
-            <p className="TituloInformacion">INFORMATE DE VARIOS TEMAS</p>
-            <img src="/imagenes/IconoInformacion.png" alt="TituloVideos" className='IconAdm'/>
+            <div className="ContainerSubTitulo">
+                <p className="TituloInformacion">INFORMATE DE VARIOS TEMAS</p>
+                <img src="/imagenes/IconoInformacion.png" alt="TituloVideos" className='IconAdm' />
+            </div>
         </div>
 
     </>
